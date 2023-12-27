f49e3281082a4c8648fd06b7b1874f29d165129a
DrNickenstein <nicola-nicotera@libero.it>
2023-12-27 19:14:12 +0100

    Initial commit

---
+++ src/main/java/io.github.drnickenstein.reworkmod/ReworkMod.java
@@ -0,0 +1,10 @@
+package io.github.drnickenstein.reworkmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ReworkMod.MODID)
+public class ReworkMod {
+
+    public static final String MODID = "rwrk";
+
+}

87e800dbbac10a9e0625388ce37bd45102a6c2e6
Eppo <eppesuig08@gmail.com>
2024-01-07 14:17:09 +0100

    Deepslate Void Crystal Ore
    - Rifatto tutto il blocco "Deepslate Void Crystal Ore";
    - Aggiunto il loot pool del Void Crystal Ore;
    - Aggiunto il modello item del Void Crystal Ore;
    - Aggiunti i file per i picconi che possono rompere il Void Crystal Ore;
    - Aggiunta la generazione del Void Crystal Ore (NON FUNZIONANTE)

--- src/main/java/io/github/drnickenstein/reworkmod/init/RwrkBlocks.java
+++ src/main/java/io/github/drnickenstein/reworkmod/init/RwrkBlocks.java
@@ -1,28 +1,29 @@
 package io.github.drnickenstein.reworkmod.init;
 
+
 import io.github.drnickenstein.reworkmod.ReworkMod;
+import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
-import net.minecraft.world.level.material.PushReaction;
+import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-import java.util.function.ToIntFunction;
-
 public class RwrkBlocks {
+
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ReworkMod.MODID);
 
-    public static final RegistryObject<Block> DEEPSLATE_VOID_CRYSTAL_ORE = BLOCKS.register("deepslate_void_crystal_ore",
-            () -> new Block(BlockBehaviour.Properties.of()
+    public static final RegistryObject<DropExperienceBlock> DEEPSLATE_VOID_CRYSTAL_ORE = BLOCKS.register("deepslate_void_crystal_ore",
+            () -> new DropExperienceBlock(ConstantInt.of(10),
+                    BlockBehaviour.Properties.of()
+                    .mapColor(MapColor.DEEPSLATE)
                     .lightLevel(state -> 10)
-                    .strength(5)
-                    .pushReaction(PushReaction.PUSH_ONLY)
                     .requiresCorrectToolForDrops()
-                    .instrument(NoteBlockInstrument.DRAGON)
-
-    ));
-
+                    .strength(4.5F, 3.0F)
+                    .sound(SoundType.DEEPSLATE)
+            )
+    );
 }
